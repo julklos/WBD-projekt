395c07b2a836910ab1834152616095932144b676
Julia Klos <julia.klos159@gmail.com>
2019-12-29 18:29:36 +0100

    npm router + worker label

--- gym/src/components/WorkerLabel.vue
+++ gym/src/components/WorkerLabel.vue
@@ -1,30 +1,27 @@
 <template>
   <b-navbar toggleable="lg" type="dark" variant="warning">
-    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    <b-navbar-brand href="#">
+      <img style="height:40px" src="../assets/logo1.png"/>
+    </b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        <b-nav-item href="#">Clients</b-nav-item>
+        <b-nav-item href="#" >Trainers</b-nav-item>
+        <b-nav-item href="#" >Workouts</b-nav-item>
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-        </b-nav-form>
-
-
         <b-nav-item-dropdown right>
           <!-- Using 'button-content' slot -->
           <template v-slot:button-content>
             <em>User</em>
           </template>
           <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          <b-dropdown-item href="#" @click="logOut">Sign Out</b-dropdown-item>
         </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-collapse>
@@ -32,11 +29,23 @@
 </template>
 
 <script>
+import {mapActions} from "vuex";
 export default {
+ methods:{
+    ...mapActions({
+      logout: "login/logout"
+    }),
+    logOut(){
+      console.log('logout');
+      this.logout()
+    }
  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.b-nav-item{
+color: rgba(255, 255, 255, 1);
+}
 </style>
--- gym/src/store/modules/login.js
+++ gym/src/store/modules/login.js
@@ -4,7 +4,7 @@
 // shape: [{ id, quantity }]
 const state = {
     user: null,
-    type: null,
+    type: "worker",
     loading: false,
     error: null
   }
@@ -28,18 +28,17 @@ const state = {
   // mutations
   const mutations = {
     setUserData (state, data) {
-        console.log(data)
       state.user = data
     },
   
     setUserType (state, data) {
-        console.log(data)
      state.type = data
     },
   
     logout (state) {
       state.user = null;
       state.type = null;
+      state.error = null;
     },
   
     setLoadingStatus (state, status) {
